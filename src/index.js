6e9c857ca382dc3e2a5149d2c912071253996d22
arestus <arestus@gmail.com>
2021-07-25 10:51:34 +0300

    Added normalize

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import 'modern-normalize/modern-normalize.css';
 
 ReactDOM.render(
   <React.StrictMode>
